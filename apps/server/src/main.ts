d2767e6a8d013a0b449d878063fbce5be8fde113
ishmaell <isiaka.ismaila@gmail.com>
2025-03-21 07:10:57 +0100

    chore(server): Enable Cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,6 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: [
+      'Accept',
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
